4e465942c04112469cb1eb76829b5f090d3ac9d1
Timothee <astagios01@gmail.com>
2022-12-06 09:40:00 +0100

    Creation fichier test plateau

---
+++ src/test/java/ps5/takenoko/Plateau/PlateauTest.java
@@ -0,0 +1,24 @@
+package ps5.takenoko.Plateau;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlateauTest {
+
+    @Test
+    void addParcelle() {
+    }
+
+    @Test
+    void endroitsPosables() {
+    }
+
+    @Test
+    void getParcelle() {
+    }
+
+    @Test
+    void isPosable() {
+    }
+}
