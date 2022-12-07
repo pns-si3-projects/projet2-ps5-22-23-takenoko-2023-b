f7bf71f37b79fd97d856f124eedfa2b52d023994
unknown <yael.bubu@gmail.com>
2022-12-07 14:51:28 +0100

    création de la branche Jeu

--- src/main/java/ps5/takenoko/Jeu/Jeu.java
+++ src/main/java/ps5/takenoko/Jeu/Jeu.java
@@ -1,5 +1,19 @@
 package ps5.takenoko.Jeu;
 
+import ps5.takenoko.Joueur.Joueur;
+import ps5.takenoko.Plateau.Parcelle;
+import ps5.takenoko.Plateau.Plateau;
+import ps5.takenoko.Plateau.Position;
+
+import java.util.Stack;
+
 public class Jeu {
 
+    public static void main(String... args) throws IllegalAccessException {
+        Plateau plateau = new Plateau();
+        Joueur p1 = new Joueur();
+        Joueur p2 = new Joueur();
+        p1.
+
+    }
 }
