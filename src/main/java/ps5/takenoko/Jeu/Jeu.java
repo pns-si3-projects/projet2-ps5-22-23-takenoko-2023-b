41ab7ec269e71f7f16337a4530f550ad0b609f89
unknown <yael.bubu@gmail.com>
2022-12-08 20:08:09 +0100

    déroulement de la partie à 1 joueur crée

--- src/main/java/ps5/takenoko/Jeu/Jeu.java
+++ src/main/java/ps5/takenoko/Jeu/Jeu.java
@@ -1,7 +1,9 @@
 package ps5.takenoko.Jeu;
 
+import ps5.takenoko.Joueur.Action;
 import ps5.takenoko.Joueur.Joueur;
 import ps5.takenoko.Plateau.Parcelle;
+import ps5.takenoko.Plateau.ParcelleInactive;
 import ps5.takenoko.Plateau.Plateau;
 import ps5.takenoko.Plateau.Position;
 
@@ -9,11 +11,36 @@ import java.util.Stack;
 
 public class Jeu {
 
-    public static void main(String... args) throws IllegalAccessException {
+    public Jeu(){
+
+    }
+
+    private void drawParcelles(Joueur j){
+        j.addParcelle(new Parcelle());
+    }
+
+    private void putParcelles(Joueur j){
+        j.putParcelle();
+    }
+
+    private void drawObjectifs(Joueur j) {
+        j.addObjectifs();
+    }
+
+    public void run(){
         Plateau plateau = new Plateau();
-        Joueur p1 = new Joueur();
-        Joueur p2 = new Joueur();
-        p1.
+        Joueur j1 = new Joueur("joueur 1");
+        int score_p1=0;
+        Joueur winner =null;
+        while(winner==null){
+            switch(j1.chooseAction()){
+                case Action.DRAW_PARCELLES -> this.drawParcelles(j1);
+                case Action.OBJECTIFS -> this.drawObjectifs(j1);
+                case Action.PUT_PARCELLES -> this.putParcelles(j1);
+            };
+            score_p1+=j1.checkObjectifs();
+            if(score_p1>=1)winner=j1;
+        }
 
     }
 }
