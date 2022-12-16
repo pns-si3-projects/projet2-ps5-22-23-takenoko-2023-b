d30bb6a2c3383b510b9c247ceb057f7dc681f32b
Timothee <astagios01@gmail.com>
2022-12-16 17:03:35 +0100

    Ce test qui ne passe pas me rend confus, je m inflige 15 points de degats

--- src/main/java/ps5/takenoko/Personnage/Deplacable.java
+++ src/main/java/ps5/takenoko/Personnage/Deplacable.java
@@ -5,19 +5,29 @@ import ps5.takenoko.Plateau.Plateau;
 import ps5.takenoko.Plateau.Position;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
-public abstract class Deplacable {
-    private Position position;
+public class Deplacable {
+    private Position position = new Position(15,15);
 
-    public ArrayList<Position> posPossibles(Plateau plateau) {
-        ArrayList<Position> positionsPosables = new ArrayList<>();
+    public Set<Position> posPossibles(Plateau plateau) {
+        Set<Position> positionsPosables = new HashSet<>();
         for(Direction d : Direction.values()) {
-            Position temp = position.getPositionByDirection(d);
-            while(!plateau.getParcelle(temp).equals(null) && plateau.getParcelle(temp).estOccupe()) {
-                positionsPosables.add(position.getPositionByDirection(d));
-                position = position.getPositionByDirection(d);
+            Position temp = this.position.getPositionByDirection(d);
+            while(plateau.getParcelle(temp) != null && plateau.getParcelle(temp).estOccupe()) {
+                positionsPosables.add(temp.getPositionByDirection(d));
+                temp = temp.getPositionByDirection(d);
             }
         }
         return positionsPosables;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
     }
 }
--- src/test/java/ps5/takenoko/Personnage/DeplacableTest.java
+++ src/test/java/ps5/takenoko/Personnage/DeplacableTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ps5.takenoko.Plateau.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,66 +14,6 @@ class DeplacableTest {
     @BeforeEach
     void CreatePlateau() {
         plateau = new Plateau();
-    }
-
-    @Test
-    void addParcelle() {
-        Parcelle par = new Parcelle();
-        Position pos = new Position(5,5);
-        assertFalse(plateau.getParcelle(pos) instanceof Parcelle);
-        plateau.addParcelle(par,pos);
-        assertTrue(plateau.getParcelle(pos) instanceof Parcelle);
-    }
-
-    @Test
-    void endroitsPosables() {
-        Set<Position> endroitsPosables = plateau.getEndroitsPosables();
-        for(int i=0; i<31; i++) {
-            for(int j=0; j<31; j++) {
-                if(endroitsPosables.contains(new Position(i,j))) {
-                    assertTrue(plateau.isPosable(new Position(i,j)));
-                } else {
-                    assertFalse(plateau.isPosable(new Position(i,j)));
-                }
-            }
-        }
-    }
-
-    @Test
-    void getParcelle() {
-        Position pos = new Position(5,5);
-        Position posorigine = new Position(15,15);
-        Parcelle par1 = new Parcelle();
-        par1.setCouleur(Couleur.JAUNE);
-
-        assertTrue(plateau.getParcelle(pos) instanceof ParcelleInactive);
-        assertTrue(plateau.getParcelle(posorigine) instanceof ParcelleOriginelle);
-
-        plateau.addParcelle(par1, pos);
-
-        assertTrue(plateau.getParcelle(pos) instanceof Parcelle);
-        assertTrue(((Parcelle) plateau.getParcelle(pos)).getCouleur() == Couleur.JAUNE);
-    }
-
-    @Test
-    void isPosable() {
-        Position pos = new Position(0,0);
-        Position posCoteCentre = new Position(14,15);
-        assertFalse(plateau.isPosable(pos));
-        assertTrue(plateau.isPosable(posCoteCentre));
-
-        Parcelle par1 = new Parcelle();
-        Parcelle par2 = new Parcelle();
-        Position pos1 = new Position(1,0);
-        Position pos2 = new Position(0,1);
-        plateau.addParcelle(par1,pos1);
-        plateau.addParcelle(par2,pos2);
-        assertTrue(plateau.isPosable(pos));
-        assertFalse(plateau.isPosable(pos1));
-    }
-
-    @Test
-    void affichePlateau() {
         try{
             plateau.addParcelle(new Parcelle(Couleur.ROSE),new Position(15,13));
             plateau.addParcelle(new Parcelle(Couleur.ROSE),new Position(14,14));
@@ -93,6 +30,16 @@ class DeplacableTest {
 
         }catch(Exception e){System.out.println(e);}
         plateau.affichePlateau();
+    }
 
+
+    @Test
+    void endroitsDeplacable() {
+        Deplacable deplacable = new Deplacable();
+        Set<Position> endroitsDeplacable = deplacable.posPossibles(plateau);
+        for(Direction d : Direction.values()) {
+            System.out.println(endroitsDeplacable);
+            assertTrue(endroitsDeplacable.contains(deplacable.getPosition().getPositionByDirection(d)));
+        }
     }
 }
