8de9e50fee84371398bb5981ea2463980be1fbcc
Timothee <astagios01@gmail.com>
2022-12-16 16:09:51 +0100

    Position deplacement possible

--- src/main/java/ps5/takenoko/Personnage/Deplacable.java
+++ src/main/java/ps5/takenoko/Personnage/Deplacable.java
@@ -1,4 +1,23 @@
 package ps5.takenoko.Personnage;
 
-public interface Deplacable {
+import ps5.takenoko.Plateau.Direction;
+import ps5.takenoko.Plateau.Plateau;
+import ps5.takenoko.Plateau.Position;
+
+import java.util.ArrayList;
+
+public abstract class Deplacable {
+    private Position position;
+
+    public ArrayList<Position> posPossibles(Plateau plateau) {
+        ArrayList<Position> positionsPosables = new ArrayList<>();
+        for(Direction d : Direction.values()) {
+            Position temp = position;
+            while(plateau.getParcelle(position.getPositionByDirection(d)).estOccupe()) {
+                positionsPosables.add(position.getPositionByDirection(d));
+                position = position.getPositionByDirection(d);
+            }
+        }
+        return positionsPosables;
+    }
 }
