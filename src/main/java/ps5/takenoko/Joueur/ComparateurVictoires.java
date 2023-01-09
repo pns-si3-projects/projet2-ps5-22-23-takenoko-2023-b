460cde1a1c2bbaf3d1d120f10cae72e788841fdb
Timothee <astagios01@gmail.com>
2023-01-09 16:06:56 +0100

    calculs prc victoire entre des bots #53

--- src/main/java/ps5/takenoko/Joueur/ComparateurVictoires.java
+++ src/main/java/ps5/takenoko/Joueur/ComparateurVictoires.java
@@ -7,11 +7,19 @@ import java.util.ArrayList;
 public class ComparateurVictoires {
 
     public static void main(String... args) throws IllegalAccessException {
-        ArrayList<Joueur> joueurs = new ArrayList<>();
-        joueurs.add(new JoueurRandom(1));
-        joueurs.add(new JoueurRandom(2));
-        Jeu j = new Jeu(joueurs);
-        j.lancer();
-        int g = j.calculGagnants().get(0).getId();
+        ArrayList<Float> points = new ArrayList<>();
+        points.add((float) 0);
+        points.add((float) 0);
+        for (int i = 0; i < 1000; i++) {
+            ArrayList<Joueur> joueurs = new ArrayList<>();
+            joueurs.add(new JoueurRandom(1));
+            joueurs.add(new JoueurRandom(2));
+            Jeu j = new Jeu(joueurs);
+            j.lancer();
+            int g = j.calculGagnants().get(0).getId();
+            points.set(g - 1, points.get(g - 1) + 1);
+        }
+        System.out.println("Joueur 1 : " + (points.get(0) / 1000 * 100)  + "%");
+        System.out.println("Joueur 2 : " + (points.get(1) / 1000 * 100) + "%");
     }
 }
