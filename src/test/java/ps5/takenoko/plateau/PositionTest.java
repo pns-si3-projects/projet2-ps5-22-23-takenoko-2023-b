452632c65308f0bce121981a6ab1915c522b4d7a
marco <marco.laclavere@etu.unice.fr>
2023-02-06 11:33:42 +0100

    new Branch Test #85

---
+++ src/test/java/ps5/takenoko/plateau/PositionTest.java
@@ -0,0 +1,21 @@
+package ps5.takenoko.plateau;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+new Position
+    @Test
+    void getPositionByDirection() {
+
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
