7780a072a0e2dee61e1ea287c07f30f0d5359e15
Timothee <astagios01@gmail.com>
2023-02-08 15:36:24 +0100

    test stats update stats #85

---
+++ src/test/java/ps5/takenoko/lanceur/StatisticsTest.java
@@ -0,0 +1,32 @@
+package ps5.takenoko.lanceur;
+
+import org.junit.jupiter.api.Test;
+import ps5.takenoko.joueur.Joueur;
+import ps5.takenoko.joueur.JoueurMoyen;
+import ps5.takenoko.joueur.JoueurRandom;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsTest {
+
+    @Test
+    void updateStats() {
+        ArrayList<Joueur> players = new ArrayList<Joueur>();
+        ArrayList<Joueur> winner = new ArrayList<Joueur>();
+        JoueurRandom joueur1 = new JoueurMoyen(0);
+
+        players.add(joueur1);
+        players.add(new JoueurRandom(1));
+
+        winner.add(joueur1);
+        Statistics stats = new Statistics(players);
+        assertEquals(stats.getScores()[0][0], 0);
+
+        stats.updateStats(winner);
+        assertEquals(stats.getScores()[0][0], 1);
+        assertEquals(stats.getScores()[1][0], 0);
+    }
+
+}
