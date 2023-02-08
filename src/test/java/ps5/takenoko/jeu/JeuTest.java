da38a77cc2ab0f9403e945300c491cea8e728cd3
Timothee <astagios01@gmail.com>
2023-02-08 09:47:53 +0100

    affiche plateau test marco #85

--- src/test/java/ps5/takenoko/jeu/JeuTest.java
+++ src/test/java/ps5/takenoko/jeu/JeuTest.java
@@ -24,13 +24,47 @@ class JeuTest {
     Plateau plateau;
     ArrayList<Joueur> players = new ArrayList<Joueur>();
 
-
     @BeforeEach
     void init(){
         players.add(new JoueurRandom(0));
         players.add(new JoueurRandom(1));
         jeu = new Jeu(players);
         plateau = new Plateau();
+    }
+
+    @Test
+    void affichePlateauTest() {
+        try{
+            plateau.addParcelle(new Parcelle(Couleur.ROSE,2),new Position(15,13));
+            plateau.addParcelle(new Parcelle(Couleur.ROSE),new Position(14,14));
+            plateau.addParcelle(new Parcelle(Couleur.VERT),new Position(15,14));
+            plateau.addParcelle(new Parcelle(Couleur.JAUNE,3),new Position(14,15));
+            plateau.addParcelle(new Parcelle(Couleur.JAUNE),new Position(16,15));
+            plateau.addParcelle(new Parcelle(Couleur.VERT),new Position(14,16));
+
+            plateau.addParcelle(new Parcelle(Couleur.ROSE,2),new Position(17,15));
+            plateau.addParcelle(new Parcelle(Couleur.ROSE),new Position(15,17));
+
+            plateau.addParcelle(new Parcelle(Couleur.ROSE),new Position(13,14));
+
+            plateau.addParcelle(new Parcelle(Couleur.ROSE),new Position(16,16));
+            plateau.addParcelle(new Parcelle(Couleur.JAUNE,1),new Position(17,16));
+            plateau.addParcelle(new Parcelle(Couleur.JAUNE,1),new Position(17,17));
+
+        }catch(Exception e){System.out.println(e);}
+        jeu = new Jeu(players);
+        jeu.setPlateau(plateau);
+        assertEquals("                                                         \u001B[30m/\u001B[0m \u001B[30m\\\u001B[0m                                                          \n" +
+                "                                                        \u001B[30m|\u001B[0m \u001B[31m2 \u001B[0m\u001B[30m|\u001B[0m                                                       \n" +
+                "                                                   \u001B[30m/\u001B[0m \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m \u001B[30m\\\u001B[0m                                                      \n" +
+                "                                                  \u001B[30m|\u001B[0m \u001B[31m0 \u001B[0m\u001B[30m|\u001B[0m \u001B[31m1 \u001B[0m| \u001B[32m1 \u001B[0m\u001B[30m|\u001B[0m                                                       \n" +
+                "                                                   \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m \\ \u001B[34m/\u001B[0m\u001B[35mJ\u001B[0m\u001B[34m\\\u001B[0m / \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m \u001B[30m\\\u001B[0m                                                  \n" +
+                "                                                    \u001B[30m|\u001B[0m \u001B[33m4 \u001B[0m\u001B[34m|\u001B[0m\u001B[34m E \u001B[0m\u001B[34m|\u001B[0m \u001B[33m1 \u001B[0m\u001B[30m|\u001B[0m \u001B[31m2 \u001B[0m\u001B[30m|\u001B[0m                                               \n" +
+                "                                                     \u001B[30m\\\u001B[0m / \u001B[34m\\\u001B[0m\u001B[35mP\u001B[0m\u001B[34m/\u001B[0m \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m \u001B[30m\\\u001B[0m                                              \n" +
+                "                                                      \u001B[30m|\u001B[0m \u001B[32m1 \u001B[0m\u001B[30m|\u001B[0m   \u001B[30m|\u001B[0m \u001B[31m0 \u001B[0m\u001B[30m|\u001B[0m \u001B[33m1 \u001B[0m\u001B[30m|\u001B[0m                                               \n" +
+                "                                                       \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m \u001B[30m\\\u001B[0m   \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m                                                \n" +
+                "                                                        \u001B[30m|\u001B[0m \u001B[31m0 \u001B[0m\u001B[30m|\u001B[0m   \u001B[30m|\u001B[0m \u001B[33m1 \u001B[0m\u001B[30m|\u001B[0m                                               \n" +
+                "                                                         \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m     \u001B[30m\\\u001B[0m \u001B[30m/\u001B[0m                                                \n",jeu.affichePlateau());
     }
 
 }
