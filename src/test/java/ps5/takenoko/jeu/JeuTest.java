fb03741e147e6cee7107945fa34d57c6d37c5fe6
Timothee <astagios01@gmail.com>
2023-02-08 14:21:48 +0100

    reparation test #85

--- src/test/java/ps5/takenoko/jeu/JeuTest.java
+++ src/test/java/ps5/takenoko/jeu/JeuTest.java
@@ -120,8 +120,9 @@ class JeuTest {
         Jeu jeu2;
         ArrayList<Joueur> players = new ArrayList<Joueur>();
         JoueurRandom joueur1 = new JoueurMoyen(0);
+        players.add(joueur1);
         players.add(new JoueurRandom(1));
-        jeu2 = new Jeu(players,plateau,new Jardinier(), new Panda(), new ObjectifList(), new ParcelleList(), 9);
+        jeu2 = new Jeu(players);
         jeu2.tourJoueur(joueur1,false);
         assertTrue(joueur1.getObjectifs().size() == 1);
     }
